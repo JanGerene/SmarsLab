ebb76744456df8928ef122d85f7924e03ddb5a07
Jan Gerene <jan.gerene@gmail.com>
2022-07-31 10:45:23 +0200

    corrected command history class

--- smars_lab.py
+++ smars_lab.py
@@ -19,37 +19,22 @@ from smars import SmarsRobot
 import smars as smars
 
 
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
-
-
 class CommandHistory:
- 
-    def __init__(self):
-        self.history = ["*** new history ***"]
+    _history = [str]
 
-    def append(self, command):
-        self.history.append(command)
+    @property
+    def history(self):
+        return self._history
+
+    def append(self, command: str):
+        self._history.append(command)
 
     def clear(self):
-        self.history = []
+        self._history = []
 
-    def get_last_ten(self):
-        return self.history[-10:]
- 
+    def get_last_ten(self)->list[str]:
+        return self._history[-10:]
+
  
 logging.basicConfig(level=logging.ERROR)
 DRIVER = smars.do_not_use_pca_driver
@@ -144,9 +129,8 @@ def background_process():
         else:
             return jsonify(result="try again")
     except Exception as error:
-        
-#         return(str(error))
-        return jsonify(result="There was an error")
+        logging.error(error)    
+    return jsonify(result="There was an error")
 
 
 @app.route('/telemetry')
@@ -163,9 +147,9 @@ def get_command_history():
         if listtype == "top10":
             return jsonify(command_history.get_last_ten())
         else:
-            return jsonify(command_history.get_history())
+            return jsonify(command_history.history())
     """ return the current command history in JSON format """
-    return jsonify(command_history.get_history())
+    return jsonify(command_history.history())
 
 
 @app.route('/setup')
